6bca8c18ee48a3029dd68b8bfd9cdab1f5a0d131
mjskarthik <96321062+mjskarthik@users.noreply.github.com>
2021-12-22 11:59:04 +0530

    Add files via upload

---
+++ Sum of 1st odd n.c
@@ -0,0 +1,12 @@
+//This is a C program to find the sum of first n odd natural numbers
+#include <stdio.h>
+void main()
+{
+	int n,sum;
+	printf("Enter a number");
+	scanf("%d",&n);
+	
+	sum=n*n;
+	printf("Sum of the first %d odd natural numbers is %d",n,sum);
+	
+}
